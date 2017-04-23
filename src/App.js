32a360ae2efa001bc7667687fa4cca4e43d9ab3a
Riki Sholiardi <sholiardy@gmail.com>
2017-04-23 14:19:28 +0700

    Story 3: Building A Sticky Sidebar In React (3/3)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import {Grid, Col, Row} from 'react-bootstrap';
-import {flatten, times} from 'lodash';
+import {flatten, times, range} from 'lodash';
+import {StickyContainer, Sticky} from 'react-sticky';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
   render() {
-    const NUMBER_OF_PARAGRAPHS = 1;
+    const NUMBER_OF_PARAGRAPHS = 15;
     const paragraphs = flatten(times(NUMBER_OF_PARAGRAPHS, (index) =>
       (
         <p key={index}>
@@ -24,6 +25,20 @@ class App extends Component {
       )
     ));
 
+    const NUMBER_OF_KITTIES = 5;
+    const kitties = range(0, NUMBER_OF_KITTIES).map((i) => {
+      return (
+        <div className="sidebar-kitties" key={i}>
+          <StickyContainer style={{zIndex: 2}}>
+            <Sticky>
+              <img src="https://placekitten.com/g/300/250" />
+            </Sticky>
+            <div style={{height: "300px"}}></div>
+          </StickyContainer>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
         <div className="App-header">
@@ -36,7 +51,7 @@ class App extends Component {
                   <Col lg={8}>
                     {paragraphs.map(paragraph => paragraph)}
                   </Col>
-                  <Col lg={4}>Sidebar</Col>
+                  <Col lg={4}>{kitties}</Col>
                 </Row>
             </Grid>
         </div>
